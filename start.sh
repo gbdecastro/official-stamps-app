d9c7d2d8507b6ad96a94440128d70380fbfe9c9b
Gabriel Castro <gabriel_castr@outlook.com.br>
2023-03-30 11:13:42 -0400

    Update gitignore

--- start.sh
+++ start.sh
@@ -4,11 +4,13 @@
 cp -f docker-compose.example.yml docker-compose.yml
 cp -f keycloak/docker-compose.example.yml keycloak/docker-compose.yml
 cp -f nest-app/.env.example  nest-app/.env.dev
-cp -f angular-app/src/environments/environment.example.ts  nest-app/.env.dev
+cp -f angular-app/cypress.config.example.ts  angular-app/cypress.config.ts
 
+# GET IP WSL
 IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | sed 's/\/.*//')
 echo "WSL IP: $IP"
 
+# UPDATE Environment Variables and Docker Compose with IP
 echo "### Updating environment variables and docker compose settings"
 
 echo "Updating in docker-compose.yml"
@@ -23,6 +25,10 @@ sed -i "s/<your-ip>/$IP/" nest-app/.env.dev
 echo "Updating in angular-app/src/environments/environment.development.ts"
 sed -i "s/<your-ip>/$IP/" angular-app/src/environments/environment.development.ts
 
+echo "Updating in angular-app/cypress.config.ts"
+sed -i "s/<your-ip>/$IP/" angular-app/cypress.config.ts
+
+# EXECUTE Docker-compose of Keycloak
 cd keycloak && docker-compose up -d
 
 echo "Waiting for setup of keycloak..."
@@ -30,7 +36,7 @@ echo "Waiting for setup of keycloak..."
 echo "What is client secret? "
 read CLIENT_SECRET
 
-
+# UPDATE Enviroments with Client Secret of Keycloak
 echo "Updating environments"
 
 cd ..
@@ -41,8 +47,10 @@ sed -i "s/<client-secret>/$CLIENT_SECRET/" nest-app/.env.dev
 echo "Updating in docker-compose.yml"
 sed -i "s/<client-secret>/$CLIENT_SECRET/" docker-compose.yml
 
+# START docker-compose global
 docker-compose up -d
 
+# BUILD SMART CONTRACT
 cd truffle-app
 
 npm i
@@ -53,4 +61,5 @@ cp -f -r build/ ../nest-app/src/infrastructure/services/contract/
 
 sed -i "s/<contract-address>/$CONTRACT_ADDRESS/" ../nest-app/.env.dev
 
+# REBUILD
 cd .. && docker-compose up -d --no-deps --build nest-app
